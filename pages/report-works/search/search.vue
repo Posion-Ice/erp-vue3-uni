4ec9ed4b08fea4460f5d07161011d811a941a762
季明栩 <icew481@126.com>
2023-01-24 10:52:29 +0800

    [ADD] 查询页面代码补充提交

---
+++ pages/report-works/search/search.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="vk-container">
+		<u-toast ref="uToast" />
+		<!-- 提示语 -->
+		<view class="vk-title">请在下方搜索框内输入或对准条形码扫码</view>
+
+		<view class="vk-search">
+			<u-search height="100" shape="square" :clearabled="true" placeholder="输入单号或对准条形码扫码" :show-action="false"
+				v-model="pageData.keyWord" @search="getProductionInfo" />
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		searchAnteriorInfo
+	} from '../../../api/odoo.js'
+	import to from 'await-to-js';
+	import {
+		reactive,
+		getCurrentInstance
+	} from 'vue';
+
+	const pageData = reactive({
+		keyWord: ''
+	});
+	const {
+		proxy
+	} = getCurrentInstance()
+
+	// 查询单据信息功能
+	async function getProductionInfo() {
+		var [error, data] = await to(searchAnteriorInfo(pageData.keyWord))
+
+		if (error) return proxy.$ut.successMsg(error)
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import 'search.scss';
+</style>
